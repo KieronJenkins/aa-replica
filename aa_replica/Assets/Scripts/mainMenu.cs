f5343b3f146bf47bef91f50161c18b605e10a111
Kieron Jenkins <KOJ1@hotmail.co.uk>
2018-03-10 22:32:51 +0000

    Initial Main Menu - 002
    First take at main menu design and interaction.

---
+++ aa_replica/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+	RaycastHit raycastTap;
+	[SerializeField]
+	private AudioSource _clickEffect;
+	[SerializeField]
+	private GameObject _soundOn;
+	[SerializeField]
+	private GameObject _soundOff;
+
+
+	// Use this for initialization
+	void Start () {
+
+		_soundOn.SetActive(true);
+		_soundOff.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		TapInput ();
+
+	}
+
+	//----------------------------------------------------------------------------------------------------
+
+	public void TapInput () {
+
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position); 
+			if (Physics.Raycast(ray, out raycastTap)) 
+			{
+				if (raycastTap.collider.tag == "SoundON") 
+				{
+					_clickEffect.volume = 0;
+					_soundOn.SetActive(false);
+					_soundOff.SetActive(true);
+				}
+
+				if (raycastTap.collider.tag == "SoundOFF") 
+				{
+					_clickEffect.volume = 0.8f;
+					_soundOn.SetActive(true);
+					_soundOff.SetActive(false);
+					_clickEffect.Play ();
+				}
+
+				if (raycastTap.collider.tag == "StartGame") 
+				{
+					_clickEffect.Play ();
+					SceneManager.LoadScene("_MainLoad");
+				}
+			}
+		}
+
+	}
+}
