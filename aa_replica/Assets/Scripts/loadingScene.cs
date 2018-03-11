a5486234e3a29c9a48e01b65f92cdf352a4a7959
Kieron Jenkins <KOJ1@hotmail.co.uk>
2018-03-11 12:47:57 +0000

    Initial Loading Screen - 003
    Initial design and coding of the loading scene for the game.

--- aa_replica/Assets/Scripts/loadingScene.cs
+++ aa_replica/Assets/Scripts/loadingScene.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class loadingScene : MonoBehaviour {
 
+	[SerializeField]
+	private int _loadingCount;
+	[SerializeField]
+	private Text _loadingText;
+	private bool _loadingOn = true;
+
 	// Use this for initialization
 	void Start () {
-		
+
+		_loadingCount = 0;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		StartGame ();
+
+		if (_loadingOn == true) { 
+			Loading ();
+		}
+
 	}
+
+	void Loading () {
+		
+		_loadingCount++;
+		_loadingText.text = _loadingCount + "%";
+
+	}
+
+	void StartGame () {
+		if (_loadingCount == 100) {
+			_loadingOn = false;
+			SceneManager.LoadScene("_MainGame");
+		}
+	}
+
 }
